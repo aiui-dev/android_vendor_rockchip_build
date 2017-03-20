2388d7c194ab960546a10ea8d7f051c76abfa916
XiNGRZ <chenxingyu92@gmail.com>
2017-03-21 01:14:32 +0800

    Hide commands

--- tasks/kernel_image.mk
+++ tasks/kernel_image.mk
@@ -2,6 +2,6 @@ ifneq ($(filter rk%,$(strip $(TARGET_BOARD_PLATFORM))),)
 
 $(INSTALLED_KERNELIMAGE_TARGET): $(MKKRNLIMG) $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Target kernel image: $@")
-	$(MKKRNLIMG) $(INSTALLED_KERNEL_TARGET) $@ > /dev/null
+	$(hide) $(MKKRNLIMG) $(INSTALLED_KERNEL_TARGET) $@ > /dev/null
 
 endif # TARGET_BOARD_PLATFORM
