698b911f667ed37bbda143a3f97ce6067b945bcf
XiNGRZ <chenxingyu92@gmail.com>
2017-03-21 00:53:23 +0800

    Disable building tasks unless it's a rk board

--- tasks/resource_image.mk
+++ tasks/resource_image.mk
@@ -1,6 +1,10 @@
+ifneq ($(filter rk%,$(strip $(TARGET_BOARD_PLATFORM))),)
+
 INSTALLED_DTBS := $(wildcard $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/*.dtb)
 
 $(INSTALLED_RESOURCEIMAGE_TARGET): $(RESOURCE_TOOL) \
 		$(INSTALLED_KERNELIMAGE_TARGET) $(INSTALLED_DTBS)
 	$(call pretty,"Target resource image: $@")
 	$(RESOURCE_TOOL) $(INSTALLED_DTBS)
+
+endif # TARGET_BOARD_PLATFORM
