d16d59c600167d74de4debf015b1706e2a18ccb2
XiNGRZ <chenxingyu92@gmail.com>
2017-03-21 03:20:09 +0800

    Correct out dir of resource.img

--- tasks/resource_image.mk
+++ tasks/resource_image.mk
@@ -5,6 +5,6 @@ INSTALLED_DTBS := $(wildcard $(KERNEL_OUT)/arch/$(TARGET_KERNEL_ARCH)/boot/dts/*
 $(INSTALLED_RESOURCEIMAGE_TARGET): $(RESOURCE_TOOL) \
 		$(INSTALLED_KERNELIMAGE_TARGET) $(INSTALLED_DTBS)
 	$(call pretty,"Target resource image: $@")
-	$(hide) $(RESOURCE_TOOL) $(INSTALLED_DTBS)
+	$(hide) pushd $(dir $@); $(RESOURCE_TOOL) $(INSTALLED_DTBS); popd
 
 endif # TARGET_BOARD_PLATFORM
