99f77241a0ff58b98ec005d482c4f3daa25272a6
XiNGRZ <chenxingyu92@gmail.com>
2017-03-22 18:05:31 +0800

    Fix no resource to build failure on the first time building kernel

--- tasks/resource_image.mk
+++ tasks/resource_image.mk
@@ -1,11 +1,10 @@
 ifneq ($(filter rk%,$(strip $(TARGET_BOARD_PLATFORM))),)
 
-INSTALLED_DTBS := $(wildcard $(KERNEL_OUT)/arch/$(TARGET_KERNEL_ARCH)/boot/dts/*.dtb)
-
-$(INSTALLED_RESOURCEIMAGE_TARGET): $(RESOURCE_TOOL) \
-		$(INSTALLED_KERNELIMAGE_TARGET) $(INSTALLED_DTBS)
+$(INSTALLED_RESOURCEIMAGE_TARGET): $(RESOURCE_TOOL) $(INSTALLED_KERNELIMAGE_TARGET)
 	$(call pretty,"Target resource image: $@")
-	$(hide) pushd $(dir $@); $(RESOURCE_TOOL) $(INSTALLED_DTBS); popd
+	$(hide) pushd $(dir $@); \
+		$(RESOURCE_TOOL) $(wildcard $(KERNEL_OUT)/arch/$(TARGET_KERNEL_ARCH)/boot/dts/*.dtb); \
+		popd
 	@echo -e ${CL_CYN}"Made resource image: $@"${CL_RST}
 
 endif # TARGET_BOARD_PLATFORM
